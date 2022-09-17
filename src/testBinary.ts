1114d61f5e96ef670235ada70d2637195c832267
Alexis Cheron <git@norech.com>
2022-09-17 23:37:42 +0400

    Update testBinary.ts

--- src/testBinary.ts
+++ src/testBinary.ts
@@ -8,19 +8,19 @@ type Eight = UnsignedNumberToByte<8>;
 type Sixteen = UnsignedNumberToByte<16>;
 type Ten = UnsignedNumberToByte<10>;
 type Twenty = UnsignedNumberToByte<20>;
-type FortyTwo = UnsignedNumberToByte<61>;
+type SixtyOne = UnsignedNumberToByte<61>;
 type OneHundredAndFiftyEight = UnsignedNumberToByte<158>;
 
 // and then we can use them in our operations
 assertMatch<ShiftOneLeft<Eight>, Sixteen>(); // => ok
 assertMatch<ShiftOneRight<Sixteen>, Eight>(); // => ok
 // assertMatch<ShiftOneLeft<Eight>, Twenty>(); // => error
-// assertMatch<ShiftOneRight<Sixteen>, FortyTwo>(); // => error
+// assertMatch<ShiftOneRight<Sixteen>, SixtyOne>(); // => error
 
 assertMatch<ShiftOneLeft<Ten>, Twenty>(); // => ok
 assertMatch<ShiftOneRight<Twenty>, Ten>(); // => ok
-// assertMatch<ShiftOneLeft<Ten>, FortyTwo>(); // => error
+// assertMatch<ShiftOneLeft<Ten>, SixtyOne>(); // => error
 // assertMatch<ShiftOneRight<Twenty>, Eight>(); // => error
 
 assertMatch<RotateOneRight<FortyTwo>, OneHundredAndFiftyEight>(); // => ok
-assertMatch<RotateOneLeft<OneHundredAndFiftyEight>, FortyTwo>(); // => ok
+assertMatch<RotateOneLeft<OneHundredAndFiftyEight>, SixtyOne>(); // => ok
